ff48c3c1be0661c63c7299365dd25c4362305218
Dhirendra <dhirensk@gmail.com>
2018-11-19 21:15:20 +0530

    changes

--- RNN/implementation.py
+++ RNN/implementation.py
@@ -8,7 +8,7 @@ Predict Google Stock Price on the start of day, based on previous 60 days stock 
  of previous 60 days, but some other parameters such as overall sensex or other indices
 """
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the dataset
@@ -25,8 +25,8 @@ training_set_scaled = sc.fit_transform(training_set)
 X_train = []
 Y_train = []
 
-len,_ = training_set.shape
-for i in range(60, len):
+length,_ = training_set.shape
+for i in range(60, length):
     X_train.append(training_set_scaled[i-60:i,0]) # its a list
     Y_train.append(training_set_scaled[i,0])  # its a list
     
@@ -55,3 +55,76 @@ regressor = Sequential()
 n_a = 50
 regressor.add(LSTM(n_a, return_sequences=True, input_shape=(X_train.shape[1],X_train.shape[2])))
 regressor.add(Dropout(rate=0.2))
+
+regressor.add(LSTM(n_a, return_sequences=True))
+regressor.add(Dropout(rate=0.2))
+
+regressor.add(LSTM(n_a, return_sequences=True))
+regressor.add(Dropout(rate=0.2))
+
+regressor.add(LSTM(n_a))
+regressor.add(Dropout(rate=0.2))
+
+regressor.add(Dense(1,activation =None))
+
+# Compiling and Fitting the data
+
+regressor.compile(optimizer='adam',loss='mean_squared_error')
+regressor.fit(x=X_train,y=Y_train,batch_size = 32,epochs=100 )
+
+# testing the model on test set
+
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:,1:2].values
+
+#Gettingt the predicted stock price
+#Important 
+"""
+The test contains Jan2017 data, inorder to generate previous 60 timesteps for the test data of Jan2017
+we need to get previous 60 records from the Training set and concatinate it with the test set.
+But we should not make use of the normalized training set for concatenation as the training set
+was fit-transformed using this set. Instead we should concatenate the dataframe objects and then 
+normalize them to generate our test set and then transform it
+"""
+# lets merge the training and test set
+#only interested in the Open Column index 1
+dataset_total = pd.concat((dataset_train['Open'],dataset_test['Open']), axis=0)
+dataset_total.shape[0]
+#notice dataset_total is of shape (1278,)
+#len(dataset_total)
+#dataset_total.size
+#dataset_total.shape[0]
+#get entire test set + last 60 records of training data
+
+inputs = dataset_total.iloc[len(dataset_total) - len(dataset_test) -60 : ].values
+
+
+#Reshape the input with 1 column
+inputs = inputs.reshape(-1,1)
+
+#tranform the input by fitting it to the standard scalar object 
+inputs = sc.transform(inputs)
+#len(inputs) = 60
+X_test = []
+for i in range(60, len(inputs)):
+    X_test.append(inputs[i-60:i,-1])
+     
+X_test = np.array(X_test)
+
+#Reshape the X_test in keras input format  size,tx,n_x
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+
+
+# predicting on test data
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+#Visualizing the Results
+
+plt.plot(real_stock_price, color='blue', label='real Google stock price ')
+plt.plot(predicted_stock_price, color='red', label='real stock price')
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
+plt.xticks(np.arange(20),np.arange(1,X_test.shape[0]))
+plt.legend()
+plt.show()

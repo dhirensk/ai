5b04e2d509dd6418e2e87e69be4316a974223d00
Dhirendra <dhirensk@gmail.com>
2018-12-03 11:03:17 +0530

    adding SOM

---
+++ SOM/Implementation.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Fraud Detection
+dataset = pd.read_csv('Credit_Card_Application.csv')
+# archive.ics.uci.edu
+# Australian credit card approval Datset
+# All Column attributes except customer id are made meaningless to protect data privacy
+

5dfebf8b103234c6d296a101d3e34e86480a13f8
Dhirendra <dhirensk@gmail.com>
2019-05-06 20:47:40 +0530

    commit

--- practice/Evaluate2.py
+++ practice/Evaluate2.py
@@ -6,16 +6,17 @@ from sklearn.metrics import f1_score
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
 x_train = np.array(x_train).reshape(x_train.shape[0],-1)
 x_test = np.array(x_test).reshape(x_test.shape[0],-1)
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=128,activation=tf.nn.relu),
-                              tf.keras.layers.Dense(10, activation = tf.nn.softmax)])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
 # loss='sparse_categorical_crossentropy'
 
 # loss=tf.losses.sparse_softmax_cross_entropy
 model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='sparse_categorical_crossentropy',
+              loss=tf.losses.sparse_softmax_cross_entropy,
               metrics=['accuracy'])
 
 hist = model.fit(x_train/255,y_train,epochs=3,verbose=1)
 # Notice we need to retain the matrix shape in evalute and predict i.e. (1,n) and not n
-loss, acc = model.evaluate(x_train[0:1]/255, y_train[0:1])
+acc = model.evaluate(x_train[0:1]/255, y_train[0:1])
 pred = model.predict(x_train[0:1]/255)
